05ae8d12dd285357383d587250c359478ee7f4b5
Lucas Pirez <lucaspirez42@gmail.com>
2023-10-09 15:37:49 -0300

    Changes in UserController

--- CryptoTracker-backend/Controllers/UserController.cs
+++ CryptoTracker-backend/Controllers/UserController.cs
@@ -1,5 +1,9 @@
 ﻿using CryptoTracker_backend.Contexts;
+using CryptoTracker_backend.DTOs;
 using CryptoTracker_backend.Entities;
+using CryptoTracker_backend.Models.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,11 +20,41 @@ namespace CryptoTracker_backend.Controllers
             _context = context;
         }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login(AuthorizationRequest authorization)
+        {
+            var result = await _context.UsersCredentials.Where(a => a.UserName == authorization.UserName).Include(a => a.User).FirstOrDefaultAsync();
+
+            if (result == null)
+            {
+
+                return new BadRequestObjectResult(new {message = "Username y/o Password incorrects" });
+            }
+
+            if (BCrypt.Net.BCrypt.Verify(authorization.Password, result.Password))
+            {
+                AuthorizationResponse response = new AuthorizationResponse
+                {
+                    Message= "Logeado con exito",
+                    userData = result.User,
+                    Token = "eue"
+                };
+
+                return new JsonResult(response);
+            }
+            else
+            {
+                return new BadRequestObjectResult(new { message = "Username y/o Password incorrects" });
+            }
+
+           
+        }
 
         [HttpGet("obtain")]
-        public async Task<ActionResult<List<User>>> Get()
+        public async Task<ActionResult<List<User>>> Geta()
         {
             var result = await _context.Users.ToListAsync();
+
             if (result == null)
             {
                 return new NotFoundResult();
@@ -28,12 +62,30 @@ namespace CryptoTracker_backend.Controllers
             return new JsonResult(result);
         }
 
-    [HttpPost("createUser")]
-    public async Task<ActionResult> Post(User user)
-    {
-       await _context.Users.AddAsync(user);
 
-       await  _context.SaveChangesAsync();
+        [HttpPost("createUser")]
+    public async Task<ActionResult> Post(UserCreacionDTO userCreation) {
+
+            var user = new User
+            {
+                LastName = userCreation.LastName,
+                FirstName = userCreation.FirstName,
+                Email = userCreation.UserEmail
+            };
+
+            string bCryptPassword = BCrypt.Net.BCrypt.HashPassword(userCreation.Password);
+                                    
+            var UserCredentials = new UserCredentials
+            {
+                Password = bCryptPassword,
+                UserName = userCreation.UserName,
+                User = user
+            };
+
+            await _context.UsersCredentials.AddAsync(UserCredentials);
+            await _context.Users.AddAsync(user);
+
+            await  _context.SaveChangesAsync();
                 return new OkResult();
             
 
