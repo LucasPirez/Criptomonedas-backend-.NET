7646cfdd714c1d46dcd746b42577b7c4149f7cd9
Lucas Pirez <lucaspirez42@gmail.com>
2023-10-10 16:42:56 -0300

    Create User DTO

---
+++ CryptoTracker-backend/DTOs/UserCreacionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoTracker_backend.DTOs
+{
+    public class UserCreacionDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
