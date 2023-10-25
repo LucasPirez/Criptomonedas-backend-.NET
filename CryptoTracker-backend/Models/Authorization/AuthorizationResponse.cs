c8d716ac893edabea1423e49184ce73eb95c2f0a
Lucas Pirez <lucaspirez42@gmail.com>
2023-10-25 19:26:07 -0300

    Add Authorization Models

--- CryptoTracker-backend/Models/Authorization/AuthorizationResponse.cs
+++ CryptoTracker-backend/Models/Authorization/AuthorizationResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace CryptoTracker_backend.Models.Authorization
+﻿using CryptoTracker_backend.Entities;
+
+namespace CryptoTracker_backend.Models.Authorization
 {
     public class AuthorizationResponse
     {
 
         public string Token { get; set; }
 
-        public bool Resultado { get; set; }
+        public User userData { get; set; }
 
         public string Message { get; set; }
     }
