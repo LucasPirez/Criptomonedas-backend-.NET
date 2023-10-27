967e560a31ca88fe3a00f5eb351b43e4fc5e763f
Lucas Pirez <lucaspirez42@gmail.com>
2023-10-27 17:42:32 -0300

    Create folder Utils, includes Roles

---
+++ CryptoTracker-backend/Utils/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CryptoTracker_backend.Utils
+{
+    public class Roles
+    {
+        public const string Admin  = "Admin";
+        public const string User  = "User";
+
+    public static bool IsValidRole(string role)
+    {
+        return role == Admin || role  == User;
+    }
+    }
+}
