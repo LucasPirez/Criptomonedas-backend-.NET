a01f49273f419f600a22f0dbf709cb1ef5faa4a0
Lucas Pirez <lucaspirez42@gmail.com>
2023-11-09 17:32:18 -0300

    create news DTO emailDTO, AlertResponseDTO

---
+++ CryptoTracker-backend/DTOs/AlertResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoTracker_backend.DTOs
+{
+    public class AlertResponseDTO
+    {
+        public string CoinId { get; set; } = string.Empty;
+       public DateTime DateCreate { get; set; }
+       public double  MinPrice { get; set; }
+       public double MaxPrice { get; set; }
+    }
+}
---
+++ CryptoTracker-backend/DTOs/EmailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoTracker_backend.DTOs
+{
+    public class EmailDTO
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string Asunt { get; set; } = string.Empty;
+        public string Content { get; set; } = string .Empty;
+    }
+}
